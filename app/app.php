2fe969dd77e8f5e61f239d5ac596b5efdff389d8
Ali Bayrak <ali.bayrak@live.fr>
2017-05-02 12:08:37 +0200

    Intégration du framework Web Bootstrap

--- app/app.php
+++ app/app.php
@@ -12,6 +12,9 @@ $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app->register(new Silex\Provider\AssetServiceProvider(), array(
+    'assets.version' => 'v1'
+));
 
 // Register services.
 $app['dao.book'] = function ($app) {
