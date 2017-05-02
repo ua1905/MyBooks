94b1e52bb3dd41a7216892628cddb73342d00ebf
Ali Bayrak <ali.bayrak@live.fr>
2017-05-02 12:00:34 +0200

    Intégration du moteur de templates Twig

--- app/routes.php
+++ app/routes.php
@@ -2,10 +2,6 @@
 
 // Home page
 $app->get('/', function () use ($app) {
-    $books = $app['dao.book']->findAll();
-
-    ob_start();             // start buffering HTML output
-    require '../views/view.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
+    $books = $app['dao.book']->findAll(); /* Requete de demande donnée */
+    return $app['twig']->render('index.html.twig', array('books' => $books));  /* Premier valeur fait reference au fichier twig. La 2eme valeur est le contenu généré */
 });
